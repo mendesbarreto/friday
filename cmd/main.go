8d01f5ce7fe4692fe3157c1c5534676263dfdd0f
Douglas <mendes-barreto@Live.com>
2022-08-08 00:50:31 -0400

    chore: Add content type to routes

--- cmd/main.go
+++ cmd/main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/mendesbarreto/friday/api/middleware"
 	"github.com/mendesbarreto/friday/api/routes"
 )
 
 func main() {
 	app := fiber.New()
 
+	app.Use(middleware.DefaultAccepts)
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, my name is F.R.I.D.A.Y, how can I help you?")
 	})
